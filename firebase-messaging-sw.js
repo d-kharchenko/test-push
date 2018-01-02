110bd2798efb38b6f5907e1b9d00dc9ceb7267dd
d-kharchenko <33715052+d-kharchenko@users.noreply.github.com>
2018-01-02 17:57:08 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,13 +1,11 @@
-// // firebase-messaging-sw.js
-// // importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-// // importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
-
-//  var config = {
-//     apiKey: "AAAALvxyJ78:APA91bFKjtFmQRluBOMjOSczBj2kmqmQv0_GgN699fy2ohJra0WPdHjICI35GXGwhkL1yxRbIjKc0dakICq6XtERZ9HMA0WovlSJGL8xBcQPkkaFru9LDEFPlUz3ZZ1R4fQlu2cnVldK",
-//     authDomain: "sushi-97014.firebaseio.com/",
-//     databaseURL: "https://sushi-97014.firebaseio.com/",
-//     storageBucket: "sushi-97014.appspot.com",
-//     messagingSenderId: "201803835327",
-//   };
-//   firebase.initializeApp(config);
-// const messaging = firebase.messaging();
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+ var config = {
+    apiKey: "AAAALvxyJ78:APA91bFKjtFmQRluBOMjOSczBj2kmqmQv0_GgN699fy2ohJra0WPdHjICI35GXGwhkL1yxRbIjKc0dakICq6XtERZ9HMA0WovlSJGL8xBcQPkkaFru9LDEFPlUz3ZZ1R4fQlu2cnVldK",
+    authDomain: "sushi-97014.firebaseio.com/",
+    databaseURL: "https://sushi-97014.firebaseio.com/",
+    storageBucket: "sushi-97014.appspot.com",
+    messagingSenderId: "201803835327",
+  };
+  firebase.initializeApp(config);
+const messaging = firebase.messaging();
