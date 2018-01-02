29d8b3b856d86defb3f62a7c4f394ad9ceaa850f
d-kharchenko <33715052+d-kharchenko@users.noreply.github.com>
2018-01-02 18:12:40 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,11 +1,12 @@
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
  var config = {
-    apiKey: "AAAALvxyJ78:APA91bFKjtFmQRluBOMjOSczBj2kmqmQv0_GgN699fy2ohJra0WPdHjICI35GXGwhkL1yxRbIjKc0dakICq6XtERZ9HMA0WovlSJGL8xBcQPkkaFru9LDEFPlUz3ZZ1R4fQlu2cnVldK",
-    authDomain: "sushi-97014.firebaseio.com/",
-    databaseURL: "https://sushi-97014.firebaseio.com/",
+    apiKey: "AIzaSyCFIg4zZ5-a5a56XWL1UC5KEc0qBJQjuog",
+    authDomain: "sushi-97014.firebaseapp.com",
+    databaseURL: "https://sushi-97014.firebaseio.com",
+    projectId: "sushi-97014",
     storageBucket: "sushi-97014.appspot.com",
-    messagingSenderId: "201803835327",
+    messagingSenderId: "201803835327"
   };
   firebase.initializeApp(config);
 const messaging = firebase.messaging();
