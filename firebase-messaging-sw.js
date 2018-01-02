acc82fbf4684fa4da3691018382d7e8dec21fef4
d-kharchenko <33715052+d-kharchenko@users.noreply.github.com>
2018-01-02 16:15:55 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,6 @@
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+// firebase-messaging-sw.js
+// importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
  var config = {
     apiKey: "AAAALvxyJ78:APA91bFKjtFmQRluBOMjOSczBj2kmqmQv0_GgN699fy2ohJra0WPdHjICI35GXGwhkL1yxRbIjKc0dakICq6XtERZ9HMA0WovlSJGL8xBcQPkkaFru9LDEFPlUz3ZZ1R4fQlu2cnVldK",
