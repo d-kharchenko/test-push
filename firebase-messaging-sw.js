1d31a960e529bcd2c3ae8011763a89cd6bf14143
Dmytro Kharchenko <Kharchenko_D@island-ukraine.com>
2018-01-02 12:14:54 +0200

    added push

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,9 @@
+// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
+
+firebase.initializeApp({
+    messagingSenderId: 'AAAALvxyJ78:APA91bFKjtFmQRluBOMjOSczBj2kmqmQv0_GgN699fy2ohJra0WPdHjICI35GXGwhkL1yxRbIjKc0dakICq6XtERZ9HMA0WovlSJGL8xBcQPkkaFru9LDEFPlUz3ZZ1R4fQlu2cnVldK'
+});
+
+const messaging = firebase.messaging();
