90fc84a7949273ec47a9a2f0bcb6ed1aa127e69f
Dmytro Kharchenko <Kharchenko_D@island-ukraine.com>
2018-01-02 15:01:47 +0200

    added manifest

---
+++ app.js
@@ -0,0 +1,8 @@
+ var config = {
+    apiKey: "AAAALvxyJ78:APA91bFKjtFmQRluBOMjOSczBj2kmqmQv0_GgN699fy2ohJra0WPdHjICI35GXGwhkL1yxRbIjKc0dakICq6XtERZ9HMA0WovlSJGL8xBcQPkkaFru9LDEFPlUz3ZZ1R4fQlu2cnVldK",
+    authDomain: "sushi-97014.firebaseio.com/",
+    databaseURL: "https://sushi-97014.firebaseio.com/",
+    storageBucket: "sushi-97014.appspot.com",
+    messagingSenderId: "201803835327",
+  };
+  firebase.initializeApp(config);
