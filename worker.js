5b08f88e31ebb74b3c44e3c3893216899a3e5cf1
d-kharchenko <33715052+d-kharchenko@users.noreply.github.com>
2018-01-03 10:45:59 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,6 @@
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/firebase-messaging-sw.js').then(function(registration) {
+  navigator.serviceWorker.register('firebase-messaging-sw.js')
+  .then(function(registration) {
     console.log('ServiceWorker registration successful with scope:',  registration.scope);
   }).catch(function(error) {
     console.log('ServiceWorker registration failed:', error);
