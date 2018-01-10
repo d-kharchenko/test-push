a1fcf5a1e23a0be2f6154f223c6bc5356aa163bb
d-kharchenko <33715052+d-kharchenko@users.noreply.github.com>
2018-01-10 10:15:33 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,12 +1,77 @@
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
- var config = {
-    apiKey: "AIzaSyATfSHLPelSo8mkaoUJt-Sc2FQaz_1mKEQ",
-    authDomain: "sushi-97014.firebaseapp.com",
-    databaseURL: "https://sushi-97014.firebaseio.com",
-    projectId: "sushi-97014",
-    storageBucket: "sushi-97014.appspot.com",
-    messagingSenderId: "201803835327"
-  };
-  firebase.initializeApp(config);
-const messaging = firebase.messaging();
+var CACHE = 'pwabuilder-precache';
+var precacheFiles = [
+      /* Add an array of files to precache for your app */
+      'https://d-kharchenko.github.io/test-push/index.html'
+    ];
+
+//Install stage sets up the cache-array to configure pre-cache content
+self.addEventListener('install', function(evt) {
+  console.log('The service worker is being installed.');
+  evt.waitUntil(precache().then(function() {
+    console.log('[ServiceWorker] Skip waiting on install');
+      return self.skipWaiting();
+
+  })
+  );
+});
+
+
+//allow sw to control of current page
+self.addEventListener('activate', function(event) {
+console.log('[ServiceWorker] Claiming clients for current page');
+      return self.clients.claim();
+
+});
+
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.'+ evt.request.url);
+  evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
+  evt.waitUntil(update(evt.request));
+});
+
+self.addEventListener('push', evt => {
+  evt.waitUntil(
+    self.registration.showNotification('Sushi', {
+      body: 'hello this my Sushi',
+      icon: 'img/sushi-256.png'
+    })
+    )
+});
+
+self.addEventListener('notificationclick', evt => {
+  evt.notification.close();
+  return clients.openWindow('http://test.island-ukraine.com/');
+});
+
+
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll(precacheFiles);
+  });
+}
+
+
+function fromCache(request) {
+  //we pull files from the cache first thing so we can show them fast
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+
+function update(request) {
+  //this is where we call the server to get the newest version of the 
+  //file to use the next time we show view
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
+
+function fromServer(request){
+  //this is the fallback if it is not in the cahche to go to the server and get it
+return fetch(request).then(function(response){ return response})
+}
