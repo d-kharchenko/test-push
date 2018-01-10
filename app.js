f3fc24cf75a092fb38372fe6f8450f43a0b746d2
d-kharchenko <33715052+d-kharchenko@users.noreply.github.com>
2018-01-10 09:57:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,43 +9,29 @@
 //     }, 1000); 
 //   } 
 
-//   // The event was re-dispatched in response to our request 
-//   // ... 
+  // The event was re-dispatched in response to our request 
+  // ... 
 // }); 
-// var config = {
-//     apiKey: "AAAALvxyJ78:APA91bFKjtFmQRluBOMjOSczBj2kmqmQv0_GgN699fy2ohJra0WPdHjICI35GXGwhkL1yxRbIjKc0dakICq6XtERZ9HMA0WovlSJGL8xBcQPkkaFru9LDEFPlUz3ZZ1R4fQlu2cnVldK",
-//     authDomain: "sushi-97014.firebaseio.com/",
-//     databaseURL: "https://sushi-97014.firebaseio.com/",
-//     storageBucket: "sushi-97014.appspot.com",
-//     messagingSenderId: "201803835327",
-//   };
-//   firebase.initializeApp(config);
-  
-//   const messaging = firebase.messaging();
+ var config = {
+    apiKey: "AIzaSyCFIg4zZ5-a5a56XWL1UC5KEc0qBJQjuog",
+    authDomain: "sushi-97014.firebaseapp.com",
+    databaseURL: "https://sushi-97014.firebaseio.com",
+    projectId: "sushi-97014",
+    storageBucket: "sushi-97014.appspot.com",
+    messagingSenderId: "201803835327"
+  };
+  firebase.initializeApp(config);
 
-//   messaging.requestPermission()
-//   .then(function(){
-//   	console.log('hello')
-//     return messaging.getToken()
-//   })
-//   .then(function(token){
-//     console.log(token)
-//   })
-//   .catch(function(err){
-//   	console.log('error message')
-//   })
+  messaging.requestPermission()
+  .then(function(){
+  	console.log('hello')
+    return messaging.getToken()
+  })
+  .then(function(token){
+    console.log(token)
+  })
+  .catch(function(err){
+  	console.log('error message')
+  })
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-    .register('pwabuilder-sw.js')
-    .then(reg => {
-      console.log('service work');
-      reg.pushManager.subscribe({userVisibleOnly: true})
-      .then(function(sub){
-        console.log('update server', sub)
-      }).catch(function(error){
-        console.log('unable', error)
-      })
-    })
 
- }
