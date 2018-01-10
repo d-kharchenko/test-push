7f05776ecc136bc1fa494e3b1497cc5529f3a725
d-kharchenko <33715052+d-kharchenko@users.noreply.github.com>
2018-01-10 10:52:16 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,3 +1,18 @@
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+ var config = {
+    apiKey: "AIzaSyCFIg4zZ5-a5a56XWL1UC5KEc0qBJQjuog",
+    authDomain: "sushi-97014.firebaseapp.com",
+    databaseURL: "https://sushi-97014.firebaseio.com",
+    projectId: "sushi-97014",
+    storageBucket: "sushi-97014.appspot.com",
+    messagingSenderId: "201803835327"
+  };
+  firebase.initializeApp(config);
+  const messaging = firebase.messaging();
+
+
+
 var CACHE = 'pwabuilder-precache';
 var precacheFiles = [
       /* Add an array of files to precache for your app */
